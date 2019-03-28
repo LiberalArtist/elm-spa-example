b50770a795184f1bd1b8a6064b934dc001f4702a
Philip McGrath <philip@philipmcgrath.com>
2019-03-28 06:56:17 -0400

    fork to expect that front- and back-ends are running on the same host

--- src/Api/Endpoint.elm
+++ src/Api/Endpoint.elm
@@ -53,8 +53,7 @@ url : List String -> List QueryParameter -> Endpoint
 url paths queryParams =
     -- NOTE: Url.Builder takes care of percent-encoding special URL characters.
     -- See https://package.elm-lang.org/packages/elm/url/latest/Url#percentEncode
-    Url.Builder.crossOrigin "https://conduit.productionready.io"
-        ("api" :: paths)
+    Url.Builder.absolute ("api" :: paths)
         queryParams
         |> Endpoint
 
